0d50d92aae556b6e49a2f7b06bce952eba437bf1
Sudarmo Adi Joyo Putro <134109668+Venc1el@users.noreply.github.com>
2023-09-24 03:51:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors(
-        origin: ["*"],
+        origin: ["https://welfarist-splicers.000webhostapp.com"],
         methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
         credentials: true,)
 );
